6ff59d0334ba8c2c68d38309e595aa5def60065f
Stanisław <s.wegner@onet.eu>
2022-12-09 23:48:56 +0100

    functions: getAllGames, addNewGame, deleteGame

--- main.js
+++ main.js
@@ -1,35 +1,83 @@
 const getAllGamesBtn = document.querySelector(".getAllGames");
 const getAllTypesBtn = document.querySelector(".getAllTypes");
 const listOfGames = document.querySelector(".listOfGames");
+const listOfTypes = document.querySelector(".listOfTypes");
+
+const addGameBtn = document.querySelector(".add-game");
+const deleteGameBtn = document.querySelector(".delete-game");
 
 const inputGameName = document.querySelector("#gameName");
 const inputTypeId = document.querySelector("#typeId");
-const inoutGameType = document.querySelector("#gameType");
+const inputGameType = document.querySelector("#gameType");
 
-const ulList = document.querySelector(".ulList");
+const ulListGames = document.querySelector(".ulListGames");
+const ulListTypes = document.querySelector(".ulListTypes");
 
 const getAllGames = () => {
-
+	ulListGames.innerHTML = "";
 
 	fetch("https://thegamechanger.azurewebsites.net/game")
 		.then((response) => response.json())
 		.then((data) => {
-
-            for(let i = 0; i < data.length; i++){
-                returnAllGames(i + 1, data[i].name, data[i].genre);
-            }
-
-			
+			for (let i = 0; i < data.length; i++) {
+				returnAllGames(i + 1, data[i].name, data[i].genre, data[i].id);
+			}
 		});
 };
 
-const returnAllGames = (number, name, type) => {
+const returnAllGames = (number, name, type, id) => {
 	const newResult = document.createElement("li");
-	newResult.innerHTML = `<p>Nr: ${number}<span> --- Tytuł:</span> ${name} <span> --- Typ:</span> ${type}</p>`;
+	newResult.innerHTML = `<p><span>Nr</span>: ${number} --- <span>Tytuł:</span> ${name}  --- <span>Typ:</span> ${type} - <span>Id:</span> ${id}</p>`;
 
-	ulList.appendChild(newResult);
-	listOfGames.style.display = 'block';
+	ulListGames.appendChild(newResult);
+	listOfGames.style.display = "block";
+};
 
+const getAllTypes = () => {
+	ulListTypes.innerHTML = "";
+
+	fetch("https://thegamechanger.azurewebsites.net/genre")
+		.then((response) => response.json())
+		.then((data) => {
+			for (let i = 0; i < data.length; i++) {
+				retrunAllTypes(i + 1, data[i].name, data[i].id);
+			}
+		});
+};
+
+const retrunAllTypes = (number, type, id) => {
+	const newResult = document.createElement("li");
+	newResult.innerHTML = `<p><span>Nr:</span> ${number}--- <span>Typ:</span> ${type} --- <span>Id:</span> ${id} </p>`;
+	ulListTypes.appendChild(newResult);
+	listOfTypes.style.display = "block";
+};
+
+const returnGameTitle = (params) => {
+	inputGameName.value;
+};
+
+const addNewGame = () => {
+	fetch(`https://thegamechanger.azurewebsites.net/game/${inputTypeId.value}`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			name: `${inputGameName.value}`,
+		}),
+	});
+};
+
+const deleteGame = () => {
+
+	fetch(`https://thegamechanger.azurewebsites.net/game/${inputTypeId.value}`, {
+		method: "DELETE",
+		
+		
+	});
 };
 
 getAllGamesBtn.addEventListener("click", getAllGames);
+getAllTypesBtn.addEventListener("click", getAllTypes);
+addGameBtn.addEventListener("click", addNewGame);
+deleteGameBtn.addEventListener('click', deleteGame);
