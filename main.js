71ed0f0136a343f0e22c96c8af9ec41f42a2689e
Stanisław <s.wegner@onet.eu>
2022-12-29 18:50:18 +0100

    pin findType button to request

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const addGameBtn = document.querySelector(".add-game");
 const deleteGameBtn = document.querySelector(".delete-game");
 const findGameBtn = document.querySelector(".find-game");
 
+const addTypeBtn = document.querySelector(".add-type");
+const findType = document.querySelector(".find-type");
+
 const inputGameName = document.querySelector("#gameName");
 const inputTypeId = document.querySelector("#typeId");
 const inputGameType = document.querySelector("#gameType");
@@ -16,6 +19,7 @@ const ulListGames = document.querySelector(".ulListGames");
 const ulListTypes = document.querySelector(".ulListTypes");
 
 const gameAlert = document.querySelector(".gameAlert");
+const typeAlert = document.querySelector(".typeAlert");
 
 const getAllGames = () => {
 	ulListGames.innerHTML = "";
@@ -36,26 +40,6 @@ const returnAllGames = (number, name, type, id) => {
 
 	ulListGames.appendChild(newResult);
 	listOfGames.style.display = "block";
-};
-
-const getAllTypes = () => {
-	ulListTypes.innerHTML = "";
-	gameAlert.style.display = "none";
-
-	fetch("https://thegamechanger.azurewebsites.net/genre")
-		.then((response) => response.json())
-		.then((data) => {
-			for (let i = 0; i < data.length; i++) {
-				retrunAllTypes(i + 1, data[i].name, data[i].id);
-			}
-		});
-};
-
-const retrunAllTypes = (number, type, id) => {
-	const newResult = document.createElement("li");
-	newResult.innerHTML = `<p><span>Nr:</span> ${number}--- <span>Typ:</span> ${type} --- <span>Id:</span> ${id} </p>`;
-	ulListTypes.appendChild(newResult);
-	listOfTypes.style.display = "block";
 };
 
 const addNewGame = () => {
@@ -87,14 +71,15 @@ const findGameByName = () => {
 	if (inputGameName.value === "") {
 		gameAlert.textContent = "Wpisz tytuł szukanej gry";
 		gameAlert.style.display = "block";
+	} else {
+		fetch(
+			`https://thegamechanger.azurewebsites.net/game/gameName/${inputGameName.value}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				returnGameByName(data.name, data.genre, data.id);
+			});
 	}
-	fetch(
-		`https://thegamechanger.azurewebsites.net/game/gameName/${inputGameName.value}`
-	)
-		.then((res) => res.json())
-		.then((data) => {
-			returnGameByName(data.name, data.genre, data.id);
-		});
 };
 
 const returnGameByName = (name, type, id) => {
@@ -105,9 +90,83 @@ const returnGameByName = (name, type, id) => {
 	listOfGames.style.display = "block";
 };
 
-
 getAllGamesBtn.addEventListener("click", getAllGames);
-getAllTypesBtn.addEventListener("click", getAllTypes);
 addGameBtn.addEventListener("click", addNewGame);
 deleteGameBtn.addEventListener("click", deleteGame);
 findGameBtn.addEventListener("click", findGameByName);
+
+
+
+
+
+// Type Of Game Functions
+
+const getAllTypes = () => {
+	ulListTypes.innerHTML = "";
+	typeAlert.style.display = "none";
+
+	fetch("https://thegamechanger.azurewebsites.net/genre")
+		.then((response) => response.json())
+		.then((data) => {
+			for (let i = 0; i < data.length; i++) {
+				retrunAllTypes(i + 1, data[i].name, data[i].id);
+			}
+		});
+};
+
+const retrunAllTypes = (number, type, id) => {
+	const newResult = document.createElement("li");
+	newResult.innerHTML = `<p><span>Nr:</span> ${number}--- <span>Typ:</span> ${type} --- <span>Id:</span> ${id} </p>`;
+	ulListTypes.appendChild(newResult);
+	listOfTypes.style.display = "block";
+};
+
+const addNewType = () => {
+	typeAlert.style.display = "none";
+
+	if(inputGameType.value === ''){
+		typeAlert.textContent = 'Wpisz nazwę nowego gatunku gry';
+		typeAlert.style.display = 'block';
+	}
+
+	fetch(`https://thegamechanger.azurewebsites.net/genre`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			name: `${inputGameType.value}`,
+		}),
+	});
+};
+
+
+const findTypeByName = () => {
+	ulListTypes.innerHTML = "";
+	typeAlert.style.display = "none";
+
+	if (inputGameType.value === "") {
+		typeAlert.textContent = "Wpisz tytuł szukanego typu gry";
+		typeAlert.style.display = "block";
+	} else {
+		fetch(
+			`https://thegamechanger.azurewebsites.net/genre/typeName/${inputGameType.value}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				returnTypeByName(data.name, data.id);
+			});
+	}
+};
+
+const returnTypeByName = (name, id) => {
+	const newResult = document.createElement("li");
+	newResult.innerHTML = `<p>--- <span>Typ:</span> ${name}  --- <span>Id:</span> ${id}</p>`;
+
+	ulListTypes.appendChild(newResult);
+	listOfTypes.style.display = "block";
+};
+
+getAllTypesBtn.addEventListener("click", getAllTypes);
+addTypeBtn.addEventListener("click", addNewType);
+findType.addEventListener("click", findTypeByName);
